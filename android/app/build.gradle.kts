3cf5ba58c2be0ff0f55d4c7444eedfae04b5740b
reyy-aja19 <raeyhanpolindra@gmail.com>
2026-04-24 20:42:14 +0700

    update build gradle

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -38,19 +38,6 @@ android {
             signingConfig = signingConfigs.getByName("debug")
         }
     }
-
-    flavorDimensions += "default"
-
-    productFlavors {
-        create("dev") {
-            dimension = "default"
-            applicationIdSuffix = ".dev"
-        }
-        create("prod") {
-            dimension = "default"
-        }
-    }
-
 }
 
 flutter {
